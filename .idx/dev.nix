09bdaf7c9d49b83697319774011ea9b3491c8d2a
dozikurmusic <dozikurmusic@gmail.com>
2026-01-07 12:03:04 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,10 +5,10 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.nodejs_20
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
   # Sets environment variables in the workspace
@@ -23,16 +23,11 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          # Serve the current directory on a simple HTTP server
+          command = ["npx" "http-server" "-p" "$PORT"];
+          manager = "web";
+        };
       };
     };
     # Workspace lifecycle hooks
